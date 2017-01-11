e9b8dd54983c4529cc32172bc34db2faf08f7c82
BapSDB <bleuzeb@etu.univ-grenoble-alpes.fr>
2017-01-11 13:32:27 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,74 +1,143 @@
 #include "main.h"
 
+void liberer_donnees_ELF (donnees_ELF *ELF, char **Donnees_ELF) {
+	
+	if (*Donnees_ELF) {
+		
+		free(*Donnees_ELF) ;
+		*Donnees_ELF = NULL ;
+		
+	}
+	
+	if (*ELF) {
+	
+		while ((*ELF)->Table_Rel) {
+
+			Section_Rel *tmp = (*ELF)->Table_Rel ;
+			(*ELF)->Table_Rel = (*ELF)->Table_Rel->succ ;
+			free(tmp) ;
+		
+		}
+		
+		while ((*ELF)->Table_Progbits) {
+
+			Section_Progbits *tmp = (*ELF)->Table_Progbits ;
+			(*ELF)->Table_Progbits = (*ELF)->Table_Progbits->succ ;
+			free(tmp) ;
+		
+		}
+
+		if ((*ELF)->Entetes_Sections) free((*ELF)->Entetes_Sections) ;
+		if ((*ELF)->Sections) free((*ELF)->Sections) ;
+		if ((*ELF)->Table_Symboles)  free((*ELF)->Table_Symboles) ;
+
+		free(*ELF) ;
+		*ELF = NULL ;
+
+	}
+	
+}
+
 
 int main (int argc, char **argv) {
 
-	if (argc >= 2) {
+	if (argc >= 3) {
 
-		FILE *f ;
+		FILE *f1, *f2 ;
 	
-		f = fopen(argv[1], "r") ;
+		f1 = fopen(argv[1], "r") ;
 		
-		if (!f) {
+		if (!f1) {
 			
-			fprintf(stderr, "Le fichier %s n'existe pas", argv [1]) ;
+			fprintf(stderr, "Le fichier %s n'a pas pu être ouvert en lecture", argv [1]) ;
 			exit (1) ;
 			
 		}
 		
-		char *Donnees_ELF = NULL ;
-		donnees_ELF ELF = NULL ;
+		f2 = fopen(argv[2], "r") ;
 		
-		lecture_fichier_ELF(&ELF, &Donnees_ELF, f, argv[1]) ;
+		if (!f2) {
+			
+			fprintf(stderr, "Le fichier %s n'a pas pu être ouvert en lecture", argv [2]) ;
+			exit (2) ;
+			
+		}
 		
-		afficher_entete_ELF (ELF->Entete_ELF) ;
+		char *Donnees_ELF_fich1 = NULL, *Donnees_ELF_fich2 = NULL ;
+		donnees_ELF ELF1 = NULL, ELF2 = NULL ;
+		
+		lecture_fichier_ELF(&ELF1, &Donnees_ELF_fich1, f1, argv[1]) ;
+		
+		lecture_fichier_ELF(&ELF2, &Donnees_ELF_fich2, f2, argv[2]) ;
+		
+		afficher_entete_ELF (ELF1->Entete_ELF) ;
 
 		printf("\n\n") ;
+		
+		//afficher_entete_ELF (ELF2->Entete_ELF) ;
+		
+		//printf("\n\n") ;
 
-		afficher_entetes_section (ELF) ;
-
+		afficher_entetes_section (ELF1) ;
+		
 		printf("\n\n") ;
+		
+		Section_Progbits * AC = ELF1->Table_Progbits ;
+		
+		while(AC) {
+			
+			printf("ind = %d, taille = %d\n", AC->ind, AC->taille) ;
+			AC = AC->succ ;
+			
+		}
+		
+		if (argc ==4) {
 
-		if (argc == 3) {
-
-				afficher_contenus_section (ELF, argv[2]) ;
+				afficher_contenus_section (ELF1, argv[3]) ;
 
 				printf("\n\n") ;
 
 		}
+		
+		afficher_entetes_section (ELF2) ;
 
-		afficher_table_symboles (ELF) ;
+		printf("\n\n") ;
+		
+		donnees_ELF ELF3 = initialiser_donnees_ELF() ;
+		char * Donnees_ELF_fich3 = NULL ;
+		
+		/*if (fusionner_progbits(ELF1, ELF2, ELF3)) {
+		
+			printf("chaine = %s\n", ELF3->Table_Chaines_ES) ;
+			
+			char indice[10] ;
+			
+			for (int i = 0 ; i < ELF3->les ; i++) {
+			
+				sprintf(indice, "%d", i) ;
+				afficher_contenus_section (ELF3, indice) ;
+				printf("\n\n") ;
+				
+			}
+			
+		*/
+			
+		afficher_table_symboles (ELF1) ;
 			
 		printf("\n\n") ;
+		
+		fusion_table_symboles (ELF1, ELF2, ELF3) ;
+
+		
 			
-		afficher_sections_reimplantation (ELF) ;
+		//afficher_sections_reimplantation (ELF) ;
 		
-		if (Donnees_ELF) {
+		liberer_donnees_ELF(&ELF1, &Donnees_ELF_fich1) ;
+		liberer_donnees_ELF(&ELF2, &Donnees_ELF_fich2) ;
+		liberer_donnees_ELF(&ELF3, &Donnees_ELF_fich3) ;
 		
-			free(Donnees_ELF) ;
-			Donnees_ELF = NULL ;
-		
-		}
-	
-		if (ELF) {
-	
-			while (ELF->Table_Rel) {
-	
-				Section_Rel *tmp = ELF->Table_Rel ;
-				ELF->Table_Rel = ELF->Table_Rel->succ ;
-				free(tmp) ;
-			
-			}
-	
-			if (ELF->Entetes_Sections) free(ELF->Entetes_Sections) ;
-			if (ELF->Sections) free(ELF->Sections) ;
-			if (ELF->Table_Symboles)  free(ELF->Table_Symboles) ;
-	
-			free(ELF) ; ELF = NULL ;
-	
-		}
-	
-		fclose(f) ;
+		fclose(f1) ;
+		fclose(f2) ;
 		
 	}
 		
--- main.h
+++ main.h
@@ -4,5 +4,7 @@
 #include "type.h"
 #include "lecture_fichier_ELF.h"
 #include "affichage_ELF.h"
+#include "fusion_progbits_ELF.h"
+#include "fusion_symboles_ELF.h"
 	
 #endif
